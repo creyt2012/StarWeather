a516f54a717fcd33b4242b18474a377cdf234da2
creyt2012 <mortarcloud@gmail.com>
2026-02-17 06:35:01 +0700

    Ultra-sync: app/Http/Controllers/User/UserDashboardController.php (06:35:01.3N)

--- app/Http/Controllers/User/UserDashboardController.php
+++ app/Http/Controllers/User/UserDashboardController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\WeatherMetric;
+use App\Models\Satellite;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class UserDashboardController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('User/Dashboard', [
+            'metrics' => WeatherMetric::latest()->limit(10)->get(),
+            'satellites' => Satellite::all()
+        ]);
+    }
 }
