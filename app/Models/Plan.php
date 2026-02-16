23113e5c31c6be8bb2bd599fdb6d08b1ecafa08c
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:15:43 +0700

    Auto-sync: app/Models/Plan.php (2026-02-17 05:15:42)

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'stripe_id',
+        'paypal_id',
+        'price',
+        'currency',
+        'interval',
+        'features',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'features' => 'array',
+        'is_active' => 'boolean',
+        'price' => 'decimal:2',
+    ];
+
+    public function tenants()
+    {
+        return $this->hasMany(Tenant::class);
+    }
 }
