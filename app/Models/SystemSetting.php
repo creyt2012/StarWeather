5b057ef3b3ab87a3de83d81dd18099214980d803
creyt2012 <mortarcloud@gmail.com>
2026-02-22 06:34:53 +0700

    Ultra-sync: app/Models/SystemSetting.php (06:34:53.3N)

--- app/Models/SystemSetting.php
+++ app/Models/SystemSetting.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SystemSetting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'group',
+        'description',
+    ];
+
+    /**
+     * Helper to get a typed setting value.
+     */
+    public static function getVal($key, $default = null)
+    {
+        $setting = self::where('key', $key)->first();
+        if (!$setting)
+            return $default;
+
+        return match ($setting->type) {
+            'boolean' => filter_var($setting->value, FILTER_VALIDATE_BOOLEAN),
+            'integer' => (int) $setting->value,
+            'json' => json_decode($setting->value, true),
+            default => $setting->value,
+        };
+    }
 }
